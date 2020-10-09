e525b720e079427aedfa662f1ce13fb34b5fde77
Melanie McCord <56701478+haterofdietcoke@users.noreply.github.com>
2020-10-09 06:08:03 -0400

    fixed issues in regression, residual plot seems off

--- main.py
+++ main.py
@@ -4,15 +4,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 import pandas as pd 
 import os
-
+def plotResiduals(arr, Y_pred):
+    plt.title("Residuals")
+    resid = residuals(arr[:,0], Y_pred[:, 0])
+    plt.plot(resid, 'ob')
+    plt.show()
 # Applying my methods to iris
 def graphIris():
     iris = arr(getPath("iris.data"))
+    names = getLabels("iris.data")
     names = getLabels(getPath("iris.data"))
+    Y_simple = simpleSingleRegression(iris, 0, 2)
+    Y_quad = simpleQuadraticRegression(iris, 0, 2)
+    plotResiduals(iris, Y_simple)
+    plotResiduals(iris, Y_quad)
     iris = normalize(iris)
-    print(iris)
+    simpleQuadraticRegression(iris, 0, 2)
+    simpleSingleRegression(iris, 0, 2)
 
 def main():
-    print("Hello world")
+    graphIris()
 if __name__ == '__main__':
     print(main())
--- regression.py
+++ regression.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 import pandas as pd 
 import os
+import math
 # Gets the file path given a file name in the format myFile.filetype
 # Note: this assumes that the file is in the same directory as main.py
 def getPath(myfile):
@@ -18,6 +19,41 @@ def arr(myFile):
     ones = np.ones( (arr.shape[0],arr.shape[1]) )
     A = np.hstack( (arr, ones) )
     return arr
+# Note: this requires
+def pairplot(arr, labels, X):
+    df = pd.DataFrame(arr, columns=labels)
+    sns.pairplot(df, x_vars=labels, y_vars=X)
+    plt.show()
+
+def scatterplot(arr, c1, c2, labels):
+    plt.xlabel(labels[c1])
+    plt.ylabel(labels[c2])
+    sns.scatterplot(arr[c1], arr[c2])
+    plt.show()
+def scaleTranslate(arr, transMatrix, scaleMatrix):
+    size = arr.shape[1]
+    T = np.eye(size)
+    T[0:-1, -1] = -transMatrix[0,0:-1]
+
+    S = np.eye(size)
+    S[0:-1,0:-1] = S[0:-1,0:-1] * (1/scaleMatrix[0,0:-1])
+    N = S @ T
+    return N
+def plot_cov(arr):
+    cov_matrix = np.cov(arr, rowvar=False)
+    plt.imshow(cov_matrix)
+    plt.show()
+def showStats(arr):
+    print("Standard Deviation: ")
+    std = np.std(arr, axis=0)
+    print(std)
+    plt.plot(std, '--or')
+    print("Covariance: ")
+    cov = np.cov(arr, rowvar=False)
+    plt.plot(cov, '--ob')
+    print("Mean")
+    means = np.mean(arr, axis=0)
+    plt.plot(means, '--om')
 
 # Gets the first row of the file and returns a list of the names of the columns
 # Assumes that the first row is separated by commas
@@ -44,28 +80,53 @@ def plot_cov(arr):
     cov_matrix = np.cov(arr, rowvar=False)
     sns.heatmap(cov_matrix, annot=True)
     plt.show()
-def simpleSingleRegression(arr, X, Y):
-    X = arr[X][:].reshape(arr.shape[1], 1)
-    Y = arr[Y][:].reshape(arr.shape[1], 1)
+def simpleQuadraticRegression(arr, X, Y):
+    X_true = arr[:,X].reshape(arr.shape[0], 1)
+    Y_true = arr[:,Y].reshape(arr.shape[0], 1)
 
     # Tack a homogeneous coordinate (H) onto the independent variable (X)
-    H = np.ones((X.shape))
-    A = np.hstack((X,H))
+    H = np.ones((X_true.shape))
+    A = np.hstack((X_true,H))
     # Tack a homogeneous coordinate (H) onto the independent variable (X)
-    H = np.ones((X.shape))
-    A = np.hstack((X,H))
-
+    H = np.ones((X_true.shape))
+    A = np.hstack((X_true ** 2, X_true, H))
+    print(A.shape, Y_true.shape)
     # Linear Regression: Y = m*x + b
-    W = np.linalg.lstsq(A, Y, rcond=None)[0]
+    W = np.linalg.inv( A.T @ A ) @ A.T @ Y_true
     Y_pred = A @ W
-    
+    print("Weights", W)
     # Visualization
-    plotLine(X, Y_pred, Y)
+    plotLine(X_true, Y_pred, Y, X, Y_true)
+    return Y_pred
+def simpleSingleRegression(arr, X, Y):
+    X_true = arr[:,X].reshape(arr.shape[0], 1)
+    Y_true = arr[:,Y].reshape(arr.shape[0], 1)
+
+    # Tack a homogeneous coordinate (H) onto the independent variable (X)
+    H = np.ones((X_true.shape))
+    A = np.hstack((X_true,H))
+    # Tack a homogeneous coordinate (H) onto the independent variable (X)
+    H = np.ones((X_true.shape))
+    A = np.hstack((X_true,H))
+    print(A.shape, Y_true.shape)
+    # Linear Regression: Y = m*x + b
+    W = np.linalg.lstsq(A, Y_true, rcond=None)[0]
+    Y_pred = A @ W
+    print("Weights: ", W)
+    # Visualization
+    plotLine(X_true, Y_pred, Y, X, Y_true)
+    return Y_pred
+def residuals(Y_true, Y_pred):
+    residuals = Y_true - Y_pred
+    print("Resid shape: ")
+    rss = np.mean(residuals ** 2)
+    print("Mean residual:", rss)
+    return residuals
 # Plots the data points and a line given an X matrix, a Y matrix and prediction
-def plotLine(X, Y_pred, Y):
+def plotLine(X_true, Y_pred, Y, X, Y_true):
     # Visualization
-    plt.plot( X[:,0], Y[:,0], "ok", label="Y" )
-    plt.plot( X[:,0], Y_pred[:,0], '-r', linewidth=3, label="Y pred")
+    plt.scatter(X_true[:,0], Y_true[:,0], c='b',)
+    plt.plot( X_true[:,0], Y_pred[:,0], '-r', linewidth=3, label="Y pred")
     plt.xlabel("X")
     plt.ylabel("Y")
     plt.grid()
@@ -73,16 +134,17 @@ def plotLine(X, Y_pred, Y):
     plt.show()
 # Testing my methods against a simple plot
 def test():
-    myTest = np.arange(81).reshape(3, 27)
-    # print(myTest)
-    myTest = myTest ** 3 + 12
+    myTest = np.arange(81).reshape(27, 3)
+    myTest[:,1] = myTest[:, 0] ** 10 + 12 + (np.sin(myTest[:, 0]) * 30)
+    plt.scatter(myTest[:,0], myTest[:,1])
+    plt.show()
+    test = myTest
     labels = ["x", "y", "z"]
-    test = normalize(myTest)
-    X = test[0][:]
-    print(X)
-    Y = test[1][:]
-    print(Y)
-    Y_pred = simpleSingleRegression(test, 1, 1)
-    print(Y_pred)
+    # test = normalize(myTest)
+    
+    Y_pred = simpleSingleRegression(test, 0, 1)
+    resid = residuals(test[:, 0], Y_pred)
+    plt.plot(test[:, 0], resid, '--ob')
+    plt.show()
 if __name__ == '__main__':
       (test())
