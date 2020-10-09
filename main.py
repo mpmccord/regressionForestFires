abfacae76c79409cf8d295014075f298a88a5bbd
Melanie McCord <56701478+haterofdietcoke@users.noreply.github.com>
2020-10-09 06:56:50 -0400

    added a graphing forest fires, need to input labels

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def plotResiduals(arr, Y_pred):
 # Applying my methods to iris
 def graphIris():
     iris = arr(getPath("iris.data"))
-    names = getLabels("iris.data")
     names = getLabels(getPath("iris.data"))
     Y_simple = simpleSingleRegression(iris, 0, 2)
     Y_quad = simpleQuadraticRegression(iris, 0, 2)
@@ -21,8 +20,27 @@ def graphIris():
     iris = normalize(iris)
     simpleQuadraticRegression(iris, 0, 2)
     simpleSingleRegression(iris, 0, 2)
-
+def graphForestFires():
+    fires = arr(getPath("forestfires.csv"))
+    names = getLabels(getPath("forestfires.csv"))
+    print(names)
+    Y_simple = simpleSingleRegression(fires, 8, 12)
+    Y_quad = simpleQuadraticRegression(fires, 8, 12)
+    plotResiduals(fires, Y_simple)
+    plotResiduals(fires, Y_quad)
+    fires = normalize(fires)
+    plt.title("Normalized fires")
+    plt.xlabel("temp")
+    plt.ylabel("area")
+    Y_quad = simpleQuadraticRegression(fires, 8, 12)
+    plt.title("Normalized fires")
+    plt.xlabel("temp")
+    plt.ylabel("area")
+    Y_simple = simpleSingleRegression(fires, 8, 12)
+    plt.title("Residuals")
+    plotResiduals(fires, Y_simple)
+    plotResiduals(fires, Y_quad)
 def main():
-    graphIris()
+    graphForestFires()
 if __name__ == '__main__':
     print(main())
